c09b646bd967a853815c33700ee6ada68360421b
Kirill Kondrashov <kondrashovks1989@gmail.com>
2026-01-16 20:55:29 +0100

    WIP

---
+++ Yoccoz.lean
@@ -0,0 +1,10 @@
+-- This module serves as the root of the `Mlc` library.
+-- Import modules here that should be built as part of the library.
+import Yoccoz.Quadratic.Complex.Basic
+import Yoccoz.Quadratic.Complex.Escape
+import Yoccoz.Quadratic.Complex.Green
+import Yoccoz.Quadratic.Complex.GreenLemmas
+import Yoccoz.Quadratic.Complex.Groetzsch
+import Yoccoz.Quadratic.Complex.Puzzle
+import Yoccoz.Quadratic.Complex.PuzzleLemmas
+import Yoccoz.Yoccoz
